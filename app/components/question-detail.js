4d09e8fc053eca488c80c983f3a18810aa88a115
Yelena <ynedelko@gamil.com>
2015-09-26 11:44:26 -0700

    add start to answer model

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model() {
+    return this.store.findAll('answer');
+  },
+
+  actions: {
+    saveAnswer(params) {
+      var newQuestion = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('question');
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -19,5 +19,16 @@ export default Ember.Route.extend({
 //     question.destroyRecord();
 //     this.transitionTo('index');
 //   }
+},
+model() {
+  return this.store.findAll('answer');
+},
+
+actions: {
+  saveAnswer(params) {
+    var newAnswer = this.store.createRecord('answer', params);
+    newQAnswer.save();
+    this.transitionTo('question');
+  },
 }
 });
